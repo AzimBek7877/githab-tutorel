e68aedc42c12de32b671997ddb575705c3d3b620
AzimBek <azim.3.11.98@gmail.com>
2021-10-26 00:35:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+"user strict";
+
+const x = 10,
+  y = 15;
+
+console.log(x + y);
+console.log(5 - 5);
+console.log(5 / 5);
+console.log(5 * 5);
+
+console.log("Hello" + "" + "World");
+
+const num = 4 + "radio";
+console.log(typeof num);
+console.log(4 + "radio");
+
+let z = 10;
+console.log(++z);
+console.log(z);
+
+//          INCER-MENT ||   DECR-HERIMENT
+let incer = 99;
+let decr = 99;
+
+console.log(--incer); //>>>98
+console.log(--decr); //>>>98
+console.log(incer++); //>>>99
+console.log(decr--); //>>>99
+console.log(incer); //>>>99
+console.log(decr); //>>>99
+
+console.log(5 / 2);
+
+const q = 15;
+const m = 7;
+
+console.log(q / m);
+console.log(2 * 3 == "6");
+console.log(2 * 3 === "6");
